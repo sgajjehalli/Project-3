858fe5af936d33882070a4f9cfc65770b83da04a
sgajjehalli <sgajjehalli@binghamton.edu>
2024-10-30 20:57:18 -0400

    Add files via upload

---
+++ simplf/AssocList.java
@@ -0,0 +1,16 @@
+
+package simplf; 
+
+public class AssocList {
+        final String name;
+        Object value;
+        final AssocList next;
+    
+        AssocList(String nameIn, Object valueIn, AssocList nextIn) {
+            name = nameIn;
+            value = valueIn;
+            next = nextIn;
+        }
+    
+}
+
---
+++ simplf/SimplfCallable.java
@@ -0,0 +1,10 @@
+package simplf; 
+
+import java.util.List;
+
+interface SimplfCallable {
+
+    Object call(Interpreter interpreter, List<Object> args);
+   
+    
+}
